df61f27944e6f8837d22e8cfe5d190cc97c0e2ca
TeppieC <teppie.c@hotmail.com>
2017-03-14 21:57:28 -0600

    real-time capture

--- main.py
+++ main.py
@@ -1,5 +1,104 @@
-def main():
-	pass
+'''
+SIZE = 1280, 720
+Distortion factor: k1=-0.0481117368, k2=0.1561547816, p1=0.0549917854, p2=-0.0129148327
+				  fx=1007.356812, fy=1033.845215, x0=642.705933, y0=517.837097, s=0.854053
+1179.50112 0.00000 642.70593 0.00000 
+0.00000 1210.51605 517.83710 0.00000 
+0.00000 0.00000 1.00000 0.00000 
+'''
+
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+class App:
+	def __init__(self, cameraMatrix):
+		self.referencePoints = []
+		self.cropping = False
+		self.current_frame = None
+		self.cameraMatrix = cameraMatrix
+
+	def chooseMarker(self):
+		cap = cv2.VideoCapture(0)
+
+		while True:
+ 
+			# Capture frame-by-frame
+			ret, frame = cap.read()
+
+			self.current_frame = frame.copy()
+			cv2.namedWindow("frame")
+			cv2.setMouseCallback("frame", self.click_and_crop)
+
+			# Our operations on the frame come here
+			#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+			
+			# Display the resulting frame
+			cv2.imshow('frame',frame)
+
+			# if there are two reference points, then crop the region of interest
+			# from teh image and display it
+			if len(self.referencePoints) == 2:
+				roi = self.current_frame[self.referencePoints[0][1]:self.referencePoints[1][1], \
+						self.referencePoints[0][0]:self.referencePoints[1][0]]
+				cv2.imshow("ROI", roi)
+				if cv2.waitKey(1) & 0xFF == ord('q'):
+					cap.release()
+					cv2.destroyAllWindows()
+					return roi
+
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				cap.release()
+				cv2.destroyAllWindows()
+				break
+
+	def click_and_crop(self, event, x, y, flags, param):
+	 
+		# if the left mouse button was clicked, record the starting
+		# (x, y) coordinates and indicate that cropping is being
+		# performed
+		if event == cv2.EVENT_LBUTTONDOWN:
+			self.referencePoints = [(x, y)]
+			self.cropping = True
+	 
+		# check to see if the left mouse button was released
+		elif event == cv2.EVENT_LBUTTONUP:
+			# record the ending (x, y) coordinates and indicate that
+			# the cropping operation is finished
+			self.referencePoints.append((x, y))
+			self.cropping = False
+	 
+			# draw a rectangle around the region of interest
+			cv2.rectangle(self.current_frame, self.referencePoints[0], self.referencePoints[1], (0, 255, 0), 2)
+			cv2.imshow("image", self.current_frame)
+
+	def processFrames(self):
+		cap = cv2.VideoCapture(0)
+
+		while True:
+ 
+			# Capture frame-by-frame
+			ret, frame = cap.read()
+			self.current_frame = frame.copy()
+
+
+
+			cv2.imshow('frame', self.current_frame)
+			cv2.imshow('marker', self.roi)
+
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				cap.release()
+				cv2.destroyAllWindows()
+				break
+
+
+	def main(self):
+		self.roi = self.chooseMarker()
+		self.processFrames()
+
+
 
 if __name__ == '__main__':
-	main()
+	app = App()
+	app.main()
--- match.py
+++ match.py
@@ -16,25 +16,47 @@ img2 = cv2.imread('2.jpg') # trainImage
 #img2 = cv2.cvtColor(img2raw, cv2.COLOR_BGR2GRAY)
 #image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+class Match:
+	def __init__(self, roi, frame):
+		self.roi = roi
+		self.frame = frame
+
+	def getCorrespondence(self):
+		self.
+''' Matching descriptors '''
+
 # Initiate SIFT detector
 sift = cv2.xfeatures2d.SIFT_create()
 # find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
+
+kp1, des1 = sift.detectAndCompute(img1,None) # kp1: keypoints from the model
+kp2, des2 = sift.detectAndCompute(img2,None) # kp2: keypoints from the frame/captured image
 FLANN_INDEX_KDTREE = 1
 index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
 search_params = dict(checks = 50)
 flann = cv2.FlannBasedMatcher(index_params, search_params)
-matches = flann.knnMatch(des1,des2,k=2)
+
+# simple nn, can be refined with cross validation
+# des1: descriptors from the model
+# des2: descriptors from the scene
+matches = flann.knnMatch(des1,des2,k=2) # [ ..., [<DMatch 0x104a34c30>, <DMatch 0x104a34c50>], ... ]
+#print(matches) 
+
 # store all the good matches as per Lowe's ratio test.
-good = []
+good = [] # good is a container for 3D points found in scene
 for m,n in matches:
 	if m.distance < 0.7*n.distance:
-		good.append(m)
+		good.append(m) # m is from the scene/captured image <DMatch 0x104a34c30>
+		#print(m)
 
+print(len(good))
 if len(good)>MIN_MATCH_COUNT:
-	src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-	dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+	src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2) # 3d vectors ? How to get the 3D points??
+	dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2) # 2d vectors
+	print(np.float32([ kp1[m.queryIdx].pt for m in good ]))
+	print(np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2))
+	#print(src_pts[0])
+	#print(dst_pts[0])
 	M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
 	matchesMask = mask.ravel().tolist()
 	h,w,d = img1.shape
@@ -44,7 +66,7 @@ if len(good)>MIN_MATCH_COUNT:
 else:
 	print("Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT))
 	matchesMask = None
-
+'''
 draw_params = dict(matchColor = (0,255,0), # draw matches in green color
 				   singlePointColor = None,
 				   matchesMask = matchesMask, # draw only inliers
@@ -54,3 +76,9 @@ img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
 #plt.show()
 plt.imshow(img3, 'gray')
 plt.show()
+'''
+
+
+''' Find out the 2D/3D correspondences '''
+
+
